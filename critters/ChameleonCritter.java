60abf78d5c3b57f8d8f1a8da78beb5f80b923957
apcs <apcs@mvhs-u528ap15.dcsdk12.org>
2014-01-23 09:24:28 -0700

    Modified ChameleonCritter

--- critters/ChameleonCritter.java
+++ critters/ChameleonCritter.java
@@ -22,6 +22,8 @@ import info.gridworld.grid.Location;
 
 import java.util.ArrayList;
 
+import java.awt.Color;
+
 /**
  * A <code>ChameleonCritter</code> takes on the color of neighboring actors as
  * it moves through the grid. <br />
@@ -29,6 +31,8 @@ import java.util.ArrayList;
  */
 public class ChameleonCritter extends Critter
 {
+
+    private static final double DARKENING_FACTOR = 0.1;
     /**
      * Randomly selects a neighbor and changes this critter's color to be the
      * same as that neighbor's. If there are no neighbors, no action is taken.
@@ -37,7 +41,10 @@ public class ChameleonCritter extends Critter
     {
         int n = actors.size();
         if (n == 0)
+        {
+            darken();
             return;
+        }
         int r = (int) (Math.random() * n);
 
         Actor other = actors.get(r);
@@ -51,5 +58,15 @@ public class ChameleonCritter extends Critter
     {
         setDirection(getLocation().getDirectionToward(loc));
         super.makeMove(loc);
+    }
+
+    public void darken()
+    {
+        Color c = getColor();
+        int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
+        int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
+        int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
+
+        setColor(new Color(red, green, blue));
     }
 }
